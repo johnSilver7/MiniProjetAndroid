515a5e045a1a64019f629c462c34714979e49df9
johnSilver7 <inspiredevelopment@hotmail.fr>
2016-01-15 10:31:51 +0100

    initialisation

--- README.md
+++
@@ -1,2 +0,0 @@
-# MiniProjetAndroid
-Cours TER
---
+++ app/src/main/java/com/m2dl/miniprojet/miniandroidter/MainTer.java
@@ -0,0 +1,13 @@
+package com.m2dl.miniprojet.miniandroidter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainTer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_ter);
+    }
+}
