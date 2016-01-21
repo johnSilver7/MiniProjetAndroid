dafc8b8ebece10281503b1c88bb0befd89122cd0
johnSilver7 <inspiredevelopment@hotmail.fr>
2016-01-21 16:57:09 +0100

    Corrections bugs sur tablette

--- app/src/main/java/com/m2dl/miniprojet/miniandroidter/activites/ConnexionActivite.java
+++ app/src/main/java/com/m2dl/miniprojet/miniandroidter/activites/ConnexionActivite.java
@@ -5,10 +5,12 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.m2dl.miniprojet.miniandroidter.domaine.Utilisateur;
 import com.m2dl.miniprojet.miniandroidter.services.FichierService;
@@ -21,6 +23,7 @@ public class ConnexionActivite extends Activity {
 
     private EditText eMdp, eLogin;
     private CheckBox cSession;
+    private TextView pasEncoreInscrit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +33,26 @@ public class ConnexionActivite extends Activity {
         eLogin = (EditText) findViewById(R.id.activite_connexion_pseudo);
         eMdp = (EditText) findViewById(R.id.activite_connexion_mdp);
         cSession = (CheckBox) findViewById(R.id.activite_connexion_box_souvenir);
+
+        pasEncoreInscrit = (TextView) findViewById(R.id.activite_connexion_textInscription);
+
+        pasEncoreInscrit.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                // have same code as onTouchEvent() (for the Activity) above
+
+                int action = event.getActionMasked();
+
+                switch (action) {
+                    case MotionEvent.ACTION_DOWN:
+                        pasEncoreInscrit();
+
+                }
+
+                return true;
+            }
+        });
     }
 
     public void garderSessionOuverte() {
@@ -44,7 +67,7 @@ public class ConnexionActivite extends Activity {
         onBackPressed();
     }
 
-    public void onClickPasEncoreInscrit(View view) {
+    public void pasEncoreInscrit() {
         startActivity(new Intent(this, InscriptionActivite.class));
     }
 
