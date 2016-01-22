3b847345b1f29e0713ecc71252b2d67f6c19892a
jak09 <marquette.quentin@orange.fr>
2016-01-22 14:59:34 +0100

    test point

--- app/src/main/java/com/m2dl/miniprojet/miniandroidter/domaine/Campus.java
+++ app/src/main/java/com/m2dl/miniprojet/miniandroidter/domaine/Campus.java
@@ -1,5 +1,7 @@
 package com.m2dl.miniprojet.miniandroidter.domaine;
 
+import android.util.Log;
+
 import java.util.List;
 
 /**
@@ -7,15 +9,20 @@ import java.util.List;
  */
 public class Campus {
 
+    private final static double TETA = 36.0;
     private static Point pHautGauche, pHautDroite, pBasGauche, pBasDroite;
 
     public static void init() {
-        Point p1 = new Point(43.568698, 1.465387);
-        Point p2 = new Point(43.560804, 1.473386);
-        Point p3 = new Point(43.564800, 1.458784);
-        Point p4 = new Point(43.556975, 1.466417);
+        Point p1 = convertirPointInformatique(new Point(43.568698, 1.465387));
+        Point p2 = convertirPointInformatique(new Point(43.560804, 1.473386));
+        Point p3 = convertirPointInformatique(new Point(43.564800, 1.458784));
+        Point p4 = convertirPointInformatique(new Point(43.556975, 1.466417));
 
-        // TODO les calculer reellement
+        Log.d("p1", "" + p1);
+        Log.d("p2", "" + p2);
+        Log.d("p3", "" + p3);
+        Log.d("p4", "" + p4);
+
         /*pHautGauche = new Point();
         pHautDroite = new Point();
         pBasGauche = new Point();
@@ -23,8 +30,44 @@ public class Campus {
     }
 
     public static Point getPointSurImage(Point pointReel) {
-        //TODO
-        return new Point(43.56, 1.46);
+        return null;
+    }
+
+    /**
+     * xReel / u1 = cos(0)
+     * u1 = xReel / cos (0)
+     *
+     * y1 / xReel = tan(0)
+     * y1 = tan(0) * xReel
+     * y2 = yReel - y1
+     * u2 / y2 = sin(0)
+     * u2 = sin(0) * y2
+     *
+     * yInformatique = u1 + u2
+     *
+     * xReel / p2 = cos (0)
+     * p2 = xReel / cos(0)
+     *
+     * p1 = yInformatique - p2
+     * p1 / xInformatique = tan(0)
+     * xInformatique = p1 / tan(0)
+     *
+     *
+     * -----
+     *
+     *
+     */
+    public static Point convertirPointInformatique(Point pointReel) {
+        double cosTETA = Math.cos(TETA);
+        double sinTETA = Math.sin(TETA);
+        double tanTETA = Math.tan(TETA);
+
+        double xReel = pointReel.getX();
+        double yReel = pointReel.gety();
+
+        double yI = (xReel / cosTETA) + (sinTETA * (yReel - (tanTETA * xReel)));
+        double xI = (yI - (xReel / cosTETA)) / tanTETA;
+        return new Point(xI, yI);
     }
 
 }
